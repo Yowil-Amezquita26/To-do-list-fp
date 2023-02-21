3fdba980434d89c69737f2ad07eb7c4d9b126f26
Yowil-Amezquita26 <yowilamezquita380@hotmail.com>
2023-02-21 15:27:41 -0400

    checking issue in profile#3

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -6,8 +6,8 @@ import authenticate  from "../services/authenticate";
 
 const UserPage = ({ setUpdate }) => {
   // setUpdate(true);
-  authenticate()
   const {user, isAuthenticated} = useAuth0();
+  authenticate()
   const {logUser, isPending, error} = getUser(
     `https://to-do-list-be.onrender.com/api/user/${user?.email}`
   );
